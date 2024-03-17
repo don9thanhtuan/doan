c91349e013df1179e866b3e4b3832b2e6fffa35d
ThinhPham03 <thinhpham2809@gmail.com>
2024-03-17 13:38:50 +0700

    fix image

--- BookStoreMobile/app/src/main/java/com/nhom4/bookstoremobile/activity/AddBook.java
+++ BookStoreMobile/app/src/main/java/com/nhom4/bookstoremobile/activity/AddBook.java
@@ -1,6 +1,5 @@
 package com.nhom4.bookstoremobile.activity;
 
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,33 +9,41 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.nhom4.bookstoremobile.MainActivity;
 import com.nhom4.bookstoremobile.R;
+import com.nhom4.bookstoremobile.entities.Book;
+import com.nhom4.bookstoremobile.retrofit.RetrofitAPI;
+import com.nhom4.bookstoremobile.service.BookService;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AddBook extends AppCompatActivity {
     private static final int PICK_IMAGE = 1;
     ImageView addBookImage;
+    Uri selectedImage;
+    BookService bookService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
 
-        addBookImage = findViewById(R.id.addBookImage);
+        bookService = RetrofitAPI.getInstance().create(BookService.class);
 
-        final EditText i_1 = findViewById(R.id.add_name);
-        final EditText i_2 = findViewById(R.id.add_price);
-        final EditText i_3 = findViewById(R.id.add_author);
-        final EditText i_4 = findViewById(R.id.add_publisher);
-        final EditText i_5 = findViewById(R.id.add_weight);
-        final EditText i_6 = findViewById(R.id.add_size);
-        final EditText i_7 = findViewById(R.id.add_stock);
-        final EditText i_8 = findViewById(R.id.add_introduction);
+        addBookImage = findViewById(R.id.addBookImage);
 
         findViewById(R.id.addBookImageButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,17 +56,25 @@ public class AddBook extends AppCompatActivity {
         findViewById(R.id.add_book_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                addBook();
             }
         });
+
+        findViewById(R.id.backButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddBook.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            selectedImage = data.getData();
             try {
                 InputStream inputStream = getContentResolver().openInputStream(selectedImage);
                 Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
@@ -68,5 +83,98 @@ public class AddBook extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void addBook() {
+        EditText nameEditText = findViewById(R.id.add_name);
+        EditText priceEditText = findViewById(R.id.add_price);
+        EditText authorEditText = findViewById(R.id.add_author);
+        EditText publisherEditText = findViewById(R.id.add_publisher);
+        EditText weightEditText = findViewById(R.id.add_weight);
+        EditText sizeEditText = findViewById(R.id.add_size);
+        EditText stockEditText = findViewById(R.id.add_stock);
+        EditText introductionEditText = findViewById(R.id.add_introduction);
+
+        String name = nameEditText.getText().toString();
+        String price = priceEditText.getText().toString();
+        String author = authorEditText.getText().toString();
+        String publisher = publisherEditText.getText().toString();
+        String weight = weightEditText.getText().toString();
+        String size = sizeEditText.getText().toString();
+        String stock = stockEditText.getText().toString();
+        String introduction = introductionEditText.getText().toString();
+
+        if (name.isEmpty() || price.isEmpty() || author.isEmpty() || publisher.isEmpty() || weight.isEmpty() ||
+                size.isEmpty() || stock.isEmpty() || introduction.isEmpty() || selectedImage == null) {
+            Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin và chọn hình ảnh", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        MultipartBody.Part imagePart = prepareFilePart(selectedImage);
+
+        Call<Book> call = bookService.addBook(imagePart,
+                RequestBody.create(MediaType.parse("text/plain"), name),
+                RequestBody.create(MediaType.parse("text/plain"), price),
+                RequestBody.create(MediaType.parse("text/plain"), author),
+                RequestBody.create(MediaType.parse("text/plain"), publisher),
+                RequestBody.create(MediaType.parse("text/plain"), weight),
+                RequestBody.create(MediaType.parse("text/plain"), size),
+                RequestBody.create(MediaType.parse("text/plain"), stock),
+                RequestBody.create(MediaType.parse("text/plain"), introduction));
+
+        call.enqueue(new Callback<Book>() {
+            @Override
+            public void onResponse(Call<Book> call, Response<Book> response) {
+                if (response.isSuccessful()) {
+                    Book book = response.body();
+                    if (book != null) {
+                        Toast.makeText(AddBook.this, "Thêm sách thành công", Toast.LENGTH_SHORT).show();
+                        clearFields();
+                    }
+                } else {
+                    Toast.makeText(AddBook.this, "Thêm sách thất bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Book> call, Throwable t) {
+                Toast.makeText(AddBook.this, "Lỗi: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private MultipartBody.Part prepareFilePart(Uri uri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            byte[] fileBytes = new byte[inputStream.available()];
+            inputStream.read(fileBytes);
+            RequestBody requestFile = RequestBody.create(MediaType.parse(getContentResolver().getType(uri)), fileBytes);
+            return MultipartBody.Part.createFormData("image", "image.jpg", requestFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void clearFields() {
+        EditText nameEditText = findViewById(R.id.add_name);
+        EditText priceEditText = findViewById(R.id.add_price);
+        EditText authorEditText = findViewById(R.id.add_author);
+        EditText publisherEditText = findViewById(R.id.add_publisher);
+        EditText weightEditText = findViewById(R.id.add_weight);
+        EditText sizeEditText = findViewById(R.id.add_size);
+        EditText stockEditText = findViewById(R.id.add_stock);
+        EditText introductionEditText = findViewById(R.id.add_introduction);
+
+        nameEditText.setText("");
+        priceEditText.setText("");
+        authorEditText.setText("");
+        publisherEditText.setText("");
+        weightEditText.setText("");
+        sizeEditText.setText("");
+        stockEditText.setText("");
+        introductionEditText.setText("");
+
+        addBookImage.setImageResource(R.drawable.imagenotavailable);
     }
 }
---
+++ BookStoreWeb/src/main/java/com/nhom1/bookstore/apis/BookResponse.java
@@ -0,0 +1,29 @@
+package com.nhom1.bookstore.apis;
+
+import com.nhom1.bookstore.entity.Book;
+
+public class BookResponse {
+    private Book book;
+    private String message;
+
+    public BookResponse(Book book, String message) {
+        this.book = book;
+        this.message = message;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
